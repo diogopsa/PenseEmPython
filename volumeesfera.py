710f0b6e26298361fc0b06986d26071a8f55649b
Diogo <65966727+diogopsa@users.noreply.github.com>
2022-06-14 22:32:53 -0300

    Volume de uma esfera
    O volume de uma esfera com raio r é (4/3)*pi*r^3. Qual o volume de uma esfera com raio 5?

---
+++ volumeesfera.py
@@ -0,0 +1,4 @@
+#o volume de uma esfera com raio r é (4/3)*pi*r^3. Qual o volume de uma esfera com raio 5?
+r=5
+v=(4/3)*3.1415*(r**3)
+print('o volume da esfera é de {:.2f} uv (unidades de volume)'.format(v))
